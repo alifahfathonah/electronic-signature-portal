878fee565df99fb8981bfd3456d1838790c85678
Tõnis Palts <tonis@maagiline.ee>
2020-12-05 00:53:46 +0200

    WIP super basic admin views, non-functional

---
+++ front-end/pages/admin/config.vue
@@ -0,0 +1,143 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Config
+        </v-card-title>
+        <v-card-text>
+          <h3>eID Easy credentials</h3>
+          <v-text-field
+            label="API key"
+            required
+          ></v-text-field>
+          <v-text-field
+            label="API secret"
+            required
+          ></v-text-field>
+          <br>
+          <h3>Access control</h3>
+          <p>Logged in users can create documents. Document creator can choose to only allow signed-in users to view and sign a document.</p>
+          <v-radio-group
+            column
+            v-model="accessControl"
+          >
+            <v-radio
+              label="Anyone can log in."
+              value="anyone"
+            ></v-radio>
+            <v-radio
+              label="Only admins can log in."
+              value="only-admins"
+            ></v-radio>
+            <v-radio
+              label="Only these people can log in:"
+              value="only-some"
+            ></v-radio>
+          </v-radio-group>
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>39304040012</v-list-item-title>
+                  <v-list-item-subtitle>Bilbo Baggins</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>38801100121</v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+          <v-text-field
+            label="Social security number"
+          >
+            <template slot="append">
+              <v-btn outlined style="margin-bottom: 6px">
+                <v-icon left>mdi-plus</v-icon>
+                Add person
+              </v-btn>
+            </template>
+          </v-text-field>
+          <br>
+          <h3>Admins</h3>
+          <p>It's a pleasure to give in to temptations when these temptations actually make your life better. That's what we do here.</p>
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account-key</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>39304040012</v-list-item-title>
+                <v-list-item-subtitle>Bilbo Baggins</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account-key</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>38801100121</v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+          <v-text-field
+            label="Social security number"
+          >
+            <template slot="append">
+              <v-btn outlined style="margin-bottom: 6px">
+                <v-icon left>mdi-plus</v-icon>
+                Add admin
+              </v-btn>
+            </template>
+          </v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/admin/config"
+          >
+            Save!
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      accessControl: 'only-some'
+    }
+  }
+}
+</script>
--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,74 +1,28 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Electronic signature portal
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <p>Here's some of that sweet talk that we're so well known for.</p>
+          <p>It's a pleasure to give in to temptations when these temptations actually make your life better. That's what we do here.</p>
+          <p>Give in to the temptation, join the forces of freedom. You can have it all - legally binding signatures AND control over your data.</p>
+          <v-text-field
+            class="input-align-right"
+            placeholder="your-company-name"
+            suffix=".portal.eideasy.com"
+          />
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn
             color="primary"
             nuxt
-            to="/inspire"
+            to="/admin/config"
           >
-            Continue
+            Let's go!
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -76,14 +30,8 @@
   </v-row>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+<style>
+.input-align-right input {
+  text-align: right;
 }
-</script>
+</style>
