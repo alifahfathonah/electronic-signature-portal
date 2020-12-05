2102b8481db65a57fc692715ee4a2e0fc13ccd8f
Tõnis Palts <tonis@maagiline.ee>
2020-12-05 14:09:13 +0200

    Create document view

--- front-end/pages/create-document.vue
+++ front-end/pages/create-document.vue
@@ -3,35 +3,28 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          Config
+          Create new document
         </v-card-title>
         <v-card-text>
-          <h3>eID Easy credentials</h3>
-          <v-text-field
-            label="API key"
-            required
-          ></v-text-field>
-          <v-text-field
-            label="API secret"
-            required
-          ></v-text-field>
-          <br>
+          Select files
+          <v-file-input
+          ></v-file-input>
           <h3>Access control</h3>
-          <p>Logged in users can create documents. Document creator can choose to only allow signed-in users to view and sign a document.</p>
+          <p>Who can sign this document?</p>
           <v-radio-group
             column
             v-model="accessControl"
           >
             <v-radio
-              label="Anyone can log in."
+              label="Anyone who has the link."
               value="anyone"
             ></v-radio>
             <v-radio
-              label="Only admins can log in."
-              value="only-admins"
+              label="Only logged in users."
+              value="only-logged-in"
             ></v-radio>
             <v-radio
-              label="Only these people can log in:"
+              label="Only these people:"
               value="only-some"
             ></v-radio>
           </v-radio-group>
@@ -75,54 +68,13 @@
             </template>
           </v-text-field>
           <br>
-          <h3>Admins</h3>
-          <p>It's a pleasure to give in to temptations when these temptations actually make your life better. That's what we do here.</p>
-          <v-list-item-group>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-account-key</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>39304040012</v-list-item-title>
-                <v-list-item-subtitle>Bilbo Baggins</v-list-item-subtitle>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-btn icon>
-                  <v-icon>mdi-close</v-icon>
-                </v-btn>
-              </v-list-item-action>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-account-key</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>38801100121</v-list-item-title>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-btn icon>
-                  <v-icon>mdi-close</v-icon>
-                </v-btn>
-              </v-list-item-action>
-            </v-list-item>
-          </v-list-item-group>
-          <v-text-field
-            label="Social security number"
-          >
-            <template slot="append">
-              <v-btn outlined style="margin-bottom: 6px">
-                <v-icon left>mdi-plus</v-icon>
-                Add admin
-              </v-btn>
-            </template>
-          </v-text-field>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn
             color="primary"
             nuxt
-            to="/create-document"
+            to="/admin/config"
           >
             Save!
           </v-btn>
