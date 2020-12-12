bd2395b98bc205568fd2f86727f2ca3c818f8a05
Margus Pala <margus.pala@gmail.com>
2020-12-12 10:41:08 +0200

    PDF preview when uploading mock

--- front-end/pages/company/_companySlug/visual-signature.vue
+++ front-end/pages/company/_companySlug/visual-signature.vue
@@ -91,7 +91,7 @@ export default {
     people: [],
     rules: {
       required: value => !!value || 'Required.',
-      email: value => {
+      email: (value) => {
         const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         return pattern.test(value) || 'Invalid e-mail.'
       }
